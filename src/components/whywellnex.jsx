6e72af84c84e84c26609fb90caecbc0dd94f7a38
unknown <Admin@Faculty01.aptech.edu>
2025-10-22 21:58:36 -0700

    finaladd

--- src/components/whywellnex.jsx
+++ src/components/whywellnex.jsx
@@ -41,7 +41,7 @@ const settings = {
   dots: true,
   infinite: true,
   speed: 800,
-  slidesToShow: 3,
+ 
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 2500,
@@ -50,7 +50,7 @@ const settings = {
   responsive: [
     { breakpoint: 1024, settings: { slidesToShow: 2 } },
     { breakpoint: 768, settings: { slidesToShow: 1 } },
-    { breakpoint: 440, settings: { slidesToShow: 1 } },
+    { breakpoint: 300, settings: { slidesToShow: 1 } },
   ],
 };
 
